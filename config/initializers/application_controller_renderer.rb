839bde4848b0f26f2daeea56e8c6c32fc24b4793
Shion Tanaka <b8219@oic.jp>
2020-05-31 00:40:01 +0900

    [Add] Refileのsecret_keyを追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b8b1188fc91ae99e006b27722a7bf1e6fb76031eccec50deb69bc7611e5d9534515484efaf362d1b1796c13adbcc3443f01656a62c8ce35f3dc132d017ed3bb4'
