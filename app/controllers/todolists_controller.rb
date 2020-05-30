b04f44ce9456e471e1001c18681d610503c4d9a0
Shion Tanaka <b8219@oic.jp>
2020-05-31 00:13:18 +0900

    [Add] updateアクション追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -20,8 +20,14 @@ class TodolistsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  private
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  private
   def list_params
     params.require(:list).permit(:title, :body)
   end
